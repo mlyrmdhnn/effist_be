d4d5a1a285456dbd9937c13f6bb85305ff240227
mlyrmdhnn <mulyaranadhan@gmail.com>
2025-11-16 17:32:33 +0700

    initial commit

---
+++ app/Http/Controllers/TestingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Testing;
+use Illuminate\Http\Request;
+
+class TestingController extends Controller
+{
+    public function coba(Request $request) {
+        return response()->json([
+            'testing' => Testing::all(),
+        ]);
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Testing;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        Testing::factory(50)->create();
+    }
+}
